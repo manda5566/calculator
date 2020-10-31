4527fd12735c515fa6b040005e9d4b01b45df1c9
manda5566 <72869096+manda5566@users.noreply.github.com>
2020-10-31 20:10:49 +0300

    idk
    really

---
+++ st.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string n = get_string("your name : ");
+    printf("%s , i'm sorrt to tell you but you're stupit\nYOU know why? you did'nt take a look AT the actual code\n",n);
+
+    
+    string m = get_string("do you want to tell me something : ");
+    printf("\n\ni appreciate that your words were so respectful :) ");
+
+}
